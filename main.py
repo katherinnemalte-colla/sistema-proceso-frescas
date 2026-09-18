49f3754bddcaa9c6de4bc92d4e2b2e744cc67f26
katherinnemalte-colla <katherinnemalte@cialta.com>
2026-09-18 10:20:59 -0500

    ventana login centrado sobre pantalla fondo

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class ControladorApp:
         self.ventana_app = None
 
         self.ventana_fondo.show()
+        QApplication.processEvents()
         self._mostrar_login()
 
     def _mostrar_login(self):
